7dcd34e1bb27c67cbf3cc4239f57718a759ac0ef
yunglean_ <71585779+yunglean4171@users.noreply.github.com>
2023-03-31 15:31:56 +0200

    v0.2.0

--- main.py
+++ main.py
@@ -2,107 +2,100 @@ from bs4 import BeautifulSoup
 import requests
 import platform
 import os
-os.environ["GIT_PYTHON_REFRESH"] = "quiet"
 import py7zr
 import git
 import shutil
-
 
 def main():
     print("Provide your license key (https://keymaster.fivem.net)")
     lickey = input("~ % ")
     print("\nProvide your steam web api key (http://steamcommunity.com/dev/apikey)")
     steamapi = input("~ % ")
-    #checking if os is windows or linux
-    os = platform.system()
-    if os == "Windows":        
+    # checking if os is Windows or Linux
+    system = platform.system()
+    if system == "Windows":
         windows(lickey, steamapi)
     else:
         linux(lickey, steamapi)
 
 def windows(lickey, steamapi):
     file_path = os.path.abspath(os.path.dirname(__file__))
-    #getting latest recommended build for linux
+    # getting latest recommended build for Windows
     URL = 'https://runtime.fivem.net/artifacts/fivem/build_server_windows/master/'
-    soup = BeautifulSoup(requests.get(URL).content, "html.parser")
+    with requests.Session() as s:
+        soup = BeautifulSoup(s.get(URL).content, "html.parser")
     relative_path = soup.find_all("div", {"class": "panel-block"})[0].next_element.next_element['href']
-    latest_stable_url = "{}{}".format(URL, relative_path[2:])
-    #creating necessery directories
-    os.mkdir(f"{file_path}/FXServer")
-    os.mkdir(f"{file_path}/FXServer/server")
-    #downloading server files
+    latest_stable_url = os.path.join(URL, relative_path[2:])
+    # creating necessary directories
+    os.makedirs(os.path.join(file_path, "FXServer", "server"), exist_ok=True)
+    # downloading server files
     req = requests.get(latest_stable_url)
-    filename = latest_stable_url.split('/')[-1]  
-    with open(f"{file_path}/FXServer/server/{filename}",'wb') as output_file:
+    filename = latest_stable_url.split('/')[-1]
+    with open(os.path.join(file_path, "FXServer", "server", filename), 'wb') as output_file:
         output_file.write(req.content)
     print('\nDownloading Completed')
-    #extracting files from server.7z
-    with py7zr.SevenZipFile(f"{file_path}/FXServer/server/server.7z", 'r') as archive:
-        archive.extractall(path=f"{file_path}/FXServer/server")
-    #deleting server.7z after extraction
-    os.remove(f"{file_path}/FXServer/server/server.7z")
+    # extracting files from server.7z
+    with py7zr.SevenZipFile(os.path.join(file_path, "FXServer", "server", "server.7z"), 'r') as archive:
+        archive.extractall(path=os.path.join(file_path, "FXServer", "server"))
+    # deleting server.7z after extraction
+    os.remove(os.path.join(file_path, "FXServer", "server", "server.7z"))
     print("\nServer files extracted correctly")
 
-    #cloning official cfx-server-data
-    git.Git(f"{file_path}/FXServer/").clone("https://github.com/citizenfx/cfx-server-data.git")
+    # cloning official cfx-server-data
+    git.Repo.clone_from("https://github.com/citizenfx/cfx-server-data.git", os.path.join(file_path, "FXServer", "cfx-server-data"))
     print("\ncfx-server-data cloned successfully")
 
-    #copying server.cfg and writing inputed data to it
-    shutil.copyfile(f"{file_path}/server.cfg", f"{file_path}/FXServer/cfx-server-data/server.cfg")
+    # copying server.cfg and writing inputted data to it
+    shutil.copyfile(os.path.join(file_path, "server.cfg"), os.path.join(file_path, "FXServer", "cfx-server-data", "server.cfg"))
 
-    with open(f"{file_path}/FXServer/cfx-server-data/server.cfg", 'r') as file:
+    with open(os.path.join(file_path, "FXServer", "cfx-server-data", "server.cfg"), 'r') as file:
         content = file.read()
-        content1 = file.read()
     # Replace string
     content = content.replace("changeme", f'"{lickey}"')
-    content1 = content.replace('set steam_webApiKey ""', f'set steam_webApiKey "{steamapi}"')
+    content = content.replace('set steam_webApiKey""", f'set steam_webApiKey "{steamapi}"')
     # Write new content in write mode 'w'
-    with open(f"{file_path}/FXServer/cfx-server-data/server.cfg", 'w') as file:
+    with open(os.path.join(file_path, "FXServer", "cfx-server-data", "server.cfg"), 'w') as file:
         file.write(content)
-        file.write(content1)
 
-    print("\nprocess completed ure good to go ;D")
+    print("\nprocess completed, you're good to go ;D")
 
 def linux(lickey, steamapi):
-    #getting latest recommended build for windows
+    # getting latest recommended build for Linux
     URL = 'https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/'
-    soup = BeautifulSoup(requests.get(URL).content, "html.parser")
+    with requests.Session() as s:
+    soup = BeautifulSoup(s.get(URL).content, "html.parser")
     relative_path = soup.find_all("div", {"class": "panel-block"})[0].next_element.next_element['href']
-    latest_stable_url = "{}{}".format(URL, relative_path[2:])
-    #creating necessery directories
-    os.mkdir("/home/ubuntu/FXServer")
-    os.mkdir("/home/ubuntu/FXServer/server")
-    #downloading server files
+    latest_stable_url = os.path.join(URL, relative_path[2:])
+    # creating necessary directories
+    os.makedirs("/home/ubuntu/FXServer/server", exist_ok=True)
+    # downloading server files
     req = requests.get(latest_stable_url)
-    filename = latest_stable_url.split('/')[-1]  
-    with open(f"/home/ubuntu/FXServer/server/{filename}",'wb') as output_file:
-        output_file.write(req.content)
+    filename = latest_stable_url.split('/')[-1]
+    with open(os.path.join("/home/ubuntu/FXServer/server", filename), 'wb') as output_file:
+    output_file.write(req.content)
     print('\nDownloading Completed')
-    #extracting files from fx.tar.xz
+    # extracting files from fx.tar.xz
     os.system("cd /home/ubuntu/FXServer/server/ && tar xf fx.tar.xz")
-    #deleting fx.tar.xz after extraction
+    # deleting fx.tar.xz after extraction
     os.remove("/home/ubuntu/FXServer/server/fx.tar.xz")
     print("\nServer files extracted correctly")
-
-    #cloning official cfx-server-data
-    git.Git("/home/ubuntu/FXServer/").clone("https://github.com/citizenfx/cfx-server-data.git")
+    # cloning official cfx-server-data
+    git.Repo.clone_from("https://github.com/citizenfx/cfx-server-data.git", "/home/ubuntu/FXServer/cfx-server-data")
     print("\ncfx-server-data cloned successfully")
 
-    #copying server.cfg and writing inputed data to it
+    # copying server.cfg and writing inputted data to it
     shutil.copyfile("/home/ubuntu/server.cfg", "/home/ubuntu/FXServer/cfx-server-data/server.cfg")
 
     with open("/home/ubuntu/FXServer/cfx-server-data/server.cfg", 'r') as file:
         content = file.read()
-        content1 = file.read()
     # Replace string
     content = content.replace("changeme", f'"{lickey}"')
-    content1 = content.replace('set steam_webApiKey ""', f'set steam_webApiKey "{steamapi}"')
+    content = content.replace('set steam_webApiKey ""', f'set steam_webApiKey "{steamapi}"')
     # Write new content in write mode 'w'
     with open("/home/ubuntu/FXServer/cfx-server-data/server.cfg", 'w') as file:
         file.write(content)
-        file.write(content1)
 
-    print("\nprocess completed ure good to go ;D")
-
-if __name__ == "__main__":
+    print("\nprocess completed, you're good to go ;D")
+if name == "main":
     main()
+                              
